a0caef729f3f6952250eee1d5f3c3dce995f0377
STL21335 <Xux98217_2>
2023-06-17 09:00:39 +0200

    final commit

--- src/main/java/org/example/NumbersConverter.java
+++ src/main/java/org/example/NumbersConverter.java
@@ -5,48 +5,146 @@ import java.util.Map;
 
 public class NumbersConverter {
 
-    private Map<Integer, String> numbers = new HashMap<Integer, String>(){{
-        put(1, "I");
-        put(4, "IV");
-        put(5, "V");
-        put(9, "IX");
-        put(10, "X");
-        put(40, "XL");
-        put(50, "L");
-        put(90, "XC");
-        put(100, "C");
-        put(400, "CD");
-        put(500, "D");
-        put(900, "CM");
-        put(1000, "M");
-    }};
 
+    private final Map<Integer, String> particularCases = new HashMap<>();
 
+    public NumbersConverter() {
+        inializeParticularCasesMap();
+    }
 
-    public String convertNumberToRomanNumeral(Integer number){
-        if(number > 0 && number < 4000 ){
-            if(numbers.containsKey(number)){
-                return numbers.get(number);
+    private void inializeParticularCasesMap() {
+        particularCases.put(4, "IV");
+        particularCases.put(5, "V");
+        particularCases.put(9, "IX");
+        particularCases.put(10, "X");
+        particularCases.put(40, "XL");
+        particularCases.put(50, "L");
+        particularCases.put(90, "XC");
+        particularCases.put(100, "C");
+        particularCases.put(400, "CD");
+        particularCases.put(500, "D");
+        particularCases.put(900, "CM");
+        particularCases.put(1000, "M");
+    }
+
+    public String convertNumberToRomanNumeral(Integer number) {
+        if (isWithinInterval(number, 0, 4000)) {
+
+            StringBuilder romanNumber = new StringBuilder();
+
+            if (particularCases.containsKey(number)) {
+                return particularCases.get(number);
             }
-            StringBuilder romanNumeral = new StringBuilder();
-            if(number < 4){
-                return addI(number, romanNumeral);
+
+            if (isWithinInterval(number, 0, 10)) {
+                return convertOneDigitNumber(number, romanNumber);
             }
 
-            if(number > 5 && number < 9){
-                romanNumeral.append("V");
-                return addI(number % 5, romanNumeral);
+            if (isWithinInterval(number, 10, 100)) {
+                return convertTwoDigitsNumber(number, romanNumber);
+            }
+
+            if (isWithinInterval(number, 100, 1000)) {
+                return convertThreeDigitsNumber(number, romanNumber);
+            }
+
+            if (number > 1000) {
+                for (int i = 0; i < (number / 1000); i++) {
+                    romanNumber.append("M");
+                }
+                return convertThreeDigitsNumber(number % 1000, romanNumber);
             }
         }
 
         return "Number out of interval";
     }
 
-    private String addI(Integer number, StringBuilder romanNumeral) {
-        for (int i = 0; i < number; i++){
-            romanNumeral.append("I");
+
+    private String convertOneDigitNumber(Integer number, StringBuilder romanNumber) {
+
+        if (particularCases.containsKey(number)) {
+            return romanNumber.append(particularCases.get(number)).toString();
         }
-        return romanNumeral.toString();
+        if (number < 4) {
+            return addI(number, romanNumber);
+        }
+        if (number > 5 && number < 9) {
+            romanNumber.append("V");
+            return addI(number % 5, romanNumber);
+        }
+        return null;
+    }
+
+    private String convertTwoDigitsNumber(Integer number, StringBuilder romanNumber) {
+        if (number < 50) {
+            getTensLessThan50OrHundredsLessThan500((number / 10) % 10, romanNumber, "XL", "X");
+        } else {
+            getTensGreaterThan50((number / 10) % 10, romanNumber);
+        }
+        return convertOneDigitNumber(number % 10, romanNumber);
+    }
+
+
+    private String convertThreeDigitsNumber(Integer number, StringBuilder romanNumber) {
+        if (number < 500) {
+            getTensLessThan50OrHundredsLessThan500((number / 100) % 10, romanNumber, "CD", "C");
+        }
+        if (number > 500) {
+            getHundredsGreaterThan500((number / 100) % 10, romanNumber);
+        }
+        return convertTwoDigitsNumber((number % 100), romanNumber);
+    }
+
+
+    private void getHundredsGreaterThan500(Integer number, StringBuilder romanNumber) {
+
+        if (number == 5) {
+            romanNumber.append("D");
+        } else {
+
+            if (number == 9) {
+                romanNumber.append("CM");
+            } else {
+                romanNumber.append("D");
+                for (int i = 0; i < number - 5; i++) {
+                    romanNumber.append("C");
+                }
+            }
+        }
+    }
+
+    public boolean isWithinInterval(Integer number, int min, int max) {
+        return number > min && number < max;
+    }
+
+    private String addI(Integer number, StringBuilder romanNumber) {
+        for (int i = 0; i < number; i++) {
+            romanNumber.append("I");
+        }
+        return romanNumber.toString();
+    }
+
+    private void getTensLessThan50OrHundredsLessThan500(Integer number, StringBuilder romanNumber,
+          String lessThan, String greaterThan) {
+        if (number == 4) {
+            romanNumber.append(lessThan);
+        } else {
+            for (int i = 0; i < number; i++) {
+                romanNumber.append(greaterThan);
+            }
+        }
+    }
+
+    private void getTensGreaterThan50(Integer number, StringBuilder romanNumber) {
+
+        if (number == 9) {
+            romanNumber.append("XC");
+        } else {
+            romanNumber.append("L");
+            for (int i = 0; i < number - 5; i++) {
+                romanNumber.append("X");
+            }
+        }
     }
 
 }
